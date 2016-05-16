eac2bc74e58a798a8f29015e11cbac67dcd175da
Wang Qun Feng <qunfengw@gmail.com>
2016-05-16 11:06:52 +0800

    handle the duplication

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,30 @@
 class Product
 
-	@@products = []
-	
-	def initialize(options={})
-		@@products << self
-	end
+  attr_reader :title
 
-	def self.all
-		@@products
-	end
+  @@products = []
+
+  def initialize(options={})
+    @title = options[:title]
+    add_to_products
+  end
+
+  def self.all
+    @@products
+  end
+
+
+  private
+
+  def add_to_products
+    if duplicate?
+      raise DuplicateProductError, "#{title} already exists."
+    else
+      @@products << self
+    end
+  end
+
+  def duplicate?
+  	@@products.map { |p| p.title  }.include?(title)
+  end
 end
