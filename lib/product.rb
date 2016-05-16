e4f1775890aff45023e862316bcfca71c8ee6fa6
Wang Qun Feng <qunfengw@gmail.com>
2016-05-16 17:43:21 +0800

    add methods to Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,30 @@
 class Product
 
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
+  def in_stock?
+  	@stock > 0
+  end
+ 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title)
+  	@@products.select { |p| p.title == title }.fetch(0)
+  end
+
+  def self.in_stock
+  	@@products.select {|p| p.in_stock?}
   end
 
 
